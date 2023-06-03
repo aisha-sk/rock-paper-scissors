22473b17127ee2fb32dea6f1326105ee668ee282
AishaSK <aishasuhail2005@gmail.com>
2023-06-03 10:09:16 +0400

    Add point variables

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 // Javascript code
+let playerPoints = 0;
+let computerPoints = 0;
+
 function getComputerChoice() {
 
     // variable i chooses an index value
@@ -21,17 +24,20 @@ function playRound(playerSelection,computerChoice) {
     
     if (playerSelection === "paper" && computerSelection === "rock") {
 
+        playerPoints += 1
         return "You Win! Paper beats Rock"
 
     }
     
     else if (playerSelection === "rock" && computerSelection === "scissors") {
 
+        playerPoints += 1
         return "You Win! Rock beats Scissors"
     }
     
     else if (playerSelection === "scissors" && computerSelection === "paper") {
 
+        playerPoints += 1
         return "You Win! Scissors beats Paper"
     } 
 
@@ -39,28 +45,42 @@ function playRound(playerSelection,computerChoice) {
     || playerSelection === "paper" && computerSelection === "paper"
     || playerSelection === "scissors" && computerSelection === "scissors") {
 
+        playerPoints += 1
+        computerPoints += 1
         return "Oops, it's a draw. You both get a point!"
     }
     else if (playerSelection === "rock" && computerSelection === "paper") {
 
+        computerPoints += 1
         return "You Lost! Paper beats Rock"
 
     }
     
     else if (playerSelection === "scissors" && computerSelection === "rock") {
 
+        computerPoints += 1
         return "You Lost! Rock beats Scissors"
     }
     
     else if (playerSelection === "paper" && computerSelection === "scissors") {
 
+        computerPoints += 1
         return "You Lost! Scissors beats Paper"
     } 
     
 }
 
-const playerSelection = "rock";
+const playerSelection = "rock"; 
 const computerSelection = getComputerChoice();
 
 console.log(playRound(playerSelection, computerSelection));
 
+// console.log(playerPoints,computerPoints);
+
+/*function game() {
+    
+    for (i = 0; i < 5; i++) {
+        
+    }
+
+}*/
